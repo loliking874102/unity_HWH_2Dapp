a739268ee65385a22c46a9adbefed70b3c07221a
jj874102 <71476463+loliking874102@users.noreply.github.com>
2021-03-19 11:50:16 +0800

    0319-3攝影機

---
+++ 2d/Assets/Script/CameraCont.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCont : MonoBehaviour
+{
+    [Header("追蹤速度"), Range(0, 50)]
+    public float speed = 1.5f;
+    [Header("上下邊界")]
+    public Vector2 LimitY = new Vector2(-5, 5);
+    [Header("左右邊界")]
+    public Vector2 LimitX = new Vector2(-5, 5);
+}
