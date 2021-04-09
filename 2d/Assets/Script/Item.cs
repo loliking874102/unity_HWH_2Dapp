c6482adb33cbefd05b8e182eb6a932fa93703f8e
jj874102 <71476463+loliking874102@users.noreply.github.com>
2021-04-09 11:54:49 +0800

    0409-5

--- 2d/Assets/Script/Item.cs
+++ 2d/Assets/Script/Item.cs
@@ -5,7 +5,7 @@ public class Item : MonoBehaviour
     [Header("掉落物品")]
     public GameObject prop;
     [Header("掉落機率"), Range(0f, 1f)]
-    public float cent = 0.7f ;
+    public float cent = 0.5f ;
     /// <summary>
     /// 隨機掉落
     /// </summary>
--- 2d/Assets/Script/TriggerArea.cs
+++ 2d/Assets/Script/TriggerArea.cs
@@ -4,4 +4,12 @@ public class TriggerArea : MonoBehaviour
 {
     [Header("控制石頭數量")]
     public GameObject[] rock;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "石頭")
+        {
+            rock[0].SetActive(false);
+            rock[1].SetActive(false);
+        }
+    }
 }
