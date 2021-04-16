b4a56a04996ff9981b4404843335d0ba3dcf228b
jj874102 <71476463+loliking874102@users.noreply.github.com>
2021-04-16 10:50:24 +0800

    0416-2

--- 2d/Assets/Script/Ai.cs
+++ 2d/Assets/Script/Ai.cs
@@ -3,8 +3,10 @@ public class Ai : MonoBehaviour
 {
     [Header("追蹤範圍"), Range(0, 8)]
     public float rangeTrack = 2;
+    [Header("攻擊範圍"), Range(0, 5)]
+    public float rangeAtt = 1;
     private Transform player;
-    
+    public float speed = 5;
     private void Start()
     {
         //搜尋玩家座標並取得物件(物件名稱).變形
@@ -18,6 +20,10 @@ public class Ai : MonoBehaviour
         Gizmos.color = new Color(0, 0, 3, 0.35f);
         //繪製形狀(中心點,半徑)
         Gizmos.DrawSphere(transform.position, rangeTrack);
+        //繪製顏色
+        Gizmos.color = new Color(1, 0, 0, 0.35f);
+        //繪製形狀(中心點,半徑)
+        Gizmos.DrawSphere(transform.position, rangeAtt);
     }
     private void Update()
     {
@@ -29,7 +35,12 @@ public class Ai : MonoBehaviour
     private void Track()
     {
                     //距離=三維向量的距離(a,b)
-        float dis = Vector3.Distance(transform.position, player.position);
-        print("距離="+dis);
+        float tra = Vector3.Distance(transform.position, player.position);
+        //如果(距離小於等於追蹤範圍)才開始追蹤
+        if(tra <= rangeTrack)
+        {
+            //物件座標 更新為 三維向量 的往前移動 (物件座標,目標座標,速度*一禎的時間)
+            transform.position=Vector3.MoveTowards(transform.position,player.position, speed*Time.deltaTime);
+        }
     }
 }
